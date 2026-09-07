2712eea2ffe7d1e1d1bb7f992407bdc635f4bfe0
İsmail Baran <ismailbaran04@gmail.com>
2026-09-07 15:40:55 +0300

    chore: initialize Soundora clean architecture with .NET 8

---
+++ Presentation/Soundora.Presentation/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Soundora.Presentation.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
